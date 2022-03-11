45737de075a8eddc9b1cfe6fa1b11e8ae515a8a4
sedrickkeh <sedrickkeh@gmail.com>
2022-03-11 17:30:43 -0500

    modify eval loop

--- train.py
+++ train.py
@@ -7,10 +7,18 @@ from stable_baselines import PPO2
 env = DummyVecEnv([lambda: MarketEnv(theta=1, lam=1, sigma=1)])
 
 model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=20000)
+model.learn(total_timesteps=200000)
 
-obs = env.reset()
-for i in range(2000):
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    env.render()
+for i in range(100):
+    obs = env.reset()
+    actions_arr, rewards_arr = [], []
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        actions_arr.append(action[0][-1])
+        rewards_arr.append(rewards[0])
+        # env.render()
+
+        if done:
+            print(f"Actions: \n{actions_arr}\n Rewards: \n{rewards_arr}\n")
+            break
